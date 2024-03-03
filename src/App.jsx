d6a9b40a80618ba96df39691fb47c2dfbacadea7
Shanawaj hossain <princesaju645@gmail.com>
2024-03-03 18:52:53 +0600

    project setup and routing

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProfilePage from "./pages/ProfilePage";
+import RegistrationPage from "./pages/RegistrationPage";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route element={<HomePage />} path="/" exact />
+        <Route element={<LoginPage />} path="/login" />
+        <Route element={<ProfilePage />} path="/me" />
+        <Route element={<RegistrationPage />} path="/register" />
+
+        <Route element={<NotFoundPage />} path="*" />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
