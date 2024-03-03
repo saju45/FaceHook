4b387c6129b3365ed21b40559cf65188054429f5
Shanawaj hossain <princesaju645@gmail.com>
2024-03-04 00:16:48 +0600

    add auth context and private routes

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,8 @@
+import { useContext, useDebugValue } from "react";
+import { AuthContext } from "../context";
+
+export const useAuth=()=>{
+    const {auth}=useContext(AuthContext);
+    useDebugValue(auth,auth=> auth?.user?"User Logged in":"User logged out")
+    return useContext(AuthContext);
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
-import React from "react";
-import Header from "../components/common/Header";
+import { Link } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
 const HomePage = () => {
+  const { auth } = useAuth();
+  console.log(auth);
   return (
     <div>
-      <Header />
+      <p>Home Page</p>
+      <Link to="/me">Go To Profile Page</Link>
     </div>
   );
 };
