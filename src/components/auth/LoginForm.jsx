97184c631d5630f78db9a375a5f0249f3439ae5c
Shanawaj hossain <princesaju645@gmail.com>
2024-03-03 23:13:56 +0600

    add header fake login and logout features

---
+++ src/components/auth/LoginForm.jsx
@@ -0,0 +1,65 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Field from "../common/Field";
+
+const LoginForm = () => {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = (formData) => {
+    console.log(formData);
+    navigate("/");
+  };
+
+  return (
+    <form
+      className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]"
+      onSubmit={handleSubmit(submitForm)}
+    >
+      <Field label="Email" error={errors.email}>
+        <input
+          className={`auth-input ${
+            errors.email ? "border-red-500" : "border-gray-200"
+          }`}
+          {...register("email", { required: "Email Id Is Required" })}
+          type="email"
+          name="email"
+          id="email"
+        />
+      </Field>
+
+      <Field label="Password" error={errors.password}>
+        <input
+          className={`auth-input ${
+            errors.password ? "border-red-500" : "border-gray-200"
+          }`}
+          {...register("password", {
+            required: "Password Id Is Required",
+            minLength: {
+              value: 8,
+              message: "Your Password must be at least 8 character",
+            },
+          })}
+          type="password"
+          name="password"
+          id="password"
+        />
+      </Field>
+
+      <Field>
+        <button
+          className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
+          type="submit"
+        >
+          Login
+        </button>
+      </Field>
+    </form>
+  );
+};
+
+export default LoginForm;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Header from "../components/common/Header";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <Header />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
