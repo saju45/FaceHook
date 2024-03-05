935f226a91f4a00e84e44b3c10f47aa699b70c23
Shanawaj hossain <princesaju645@gmail.com>
2024-03-05 19:18:15 +0600

    Profile Post

---
+++ src/components/posts/PostCard.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import PostAction from "./PostAction";
+import PostBody from "./PostBody";
+import PostComment from "./PostComment";
+import PostHeader from "./PostHeader";
+
+const PostCard = ({ post }) => {
+  return (
+    <article className="card mt-6 lg:mt-8">
+      <PostHeader post={post} />
+      <PostBody poster={post?.image} content={post?.content} />
+      <PostAction postId={post?.id} commentCount={post?.comments?.length} />
+      <PostComment post={post} />
+    </article>
+  );
+};
+
+export default PostCard;
---
+++ src/components/posts/PostComment.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { useAvatar } from "../../hooks/useAvatar";
+import PostCommentList from "./PostCommentList";
+
+const PostComments = ({ post }) => {
+  const { avatarURL } = useAvatar(post);
+  const [showComment, setShowCommet] = useState(false);
+
+  return (
+    <div>
+      <div className="flex-center mb-3 gap-2 lg:gap-4">
+        <img
+          className="max-w-7 max-h-7 rounded-full lg:max-h-[34px] lg:max-w-[34px]"
+          src={avatarURL}
+          alt="avatar"
+        />
+
+        <div className="flex-1">
+          <input
+            type="text"
+            className="h-8 w-full rounded-full bg-lighterDark px-4 text-xs focus:outline-none sm:h-[38px]"
+            name="post"
+            id="post"
+            placeholder="What's on your mind?"
+          />
+        </div>
+      </div>
+      <div className="mt-4">
+        <button
+          className="text-gray-300 max-md:text-sm"
+          onClick={() => setShowCommet(!showComment)}
+        >
+          All Comment ▾
+        </button>
+      </div>
+
+      {showComment && <PostCommentList comments={post?.comments} />}
+    </div>
+  );
+};
+
+export default PostComments;
